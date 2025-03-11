255945976837956b8f21df79e1a9337d558791ce
halyna-baklanova <novabalka@gmail.com>
2025-03-11 11:37:14 +0200

    add html

--- learn/urls.py
+++ learn/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from learn.views import TaskViewSet
+from learn.views import TaskViewSet, home_view
 
 router = routers.DefaultRouter()
 router.register("learn", TaskViewSet)
 
-urlpatterns = [path("", include(router.urls))]
+urlpatterns = [
+    path("home/", home_view, name="home"),
+    path("", include(router.urls)),
+]
 
 app_name = "task"
--- learn/views.py
+++ learn/views.py
@@ -1,8 +1,12 @@
-from rest_framework import viewsets, mixins
+from rest_framework import viewsets
+from django.shortcuts import render
 
 from learn.models import Task
 from learn.serializers import TaskSerializer
 
+
+def home_view(request):
+    return render(request, "index.html")
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
