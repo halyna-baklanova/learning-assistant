c628a27e532e3a3c773df8a5588b65509128acd7
halyna-baklanova <novabalka@gmail.com>
2025-03-15 10:27:42 +0200

    create random page

--- learn/models.py
+++ learn/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Task(models.Model):
     question = models.TextField()
     answer = models.TextField()
--- learn/views.py
+++ learn/views.py
@@ -1,13 +1,26 @@
-from rest_framework import viewsets
-from django.shortcuts import render
+# learn/views.py
+from rest_framework import viewsets, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+import random
 
 from learn.models import Task
 from learn.serializers import TaskSerializer
 
 
-def home_view(request):
-    return render(request, "index.html")
-
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+
+
+class RandomQuestionView(APIView):
+    def get(self, request):
+        questions = Task.objects.all()
+        if not questions.exists():
+            return Response(
+                {"detail": "Питань немає в базі."}, status=status.HTTP_404_NOT_FOUND
+            )
+
+        random_question = random.choice(questions)
+        serializer = TaskSerializer(random_question)
+        return Response(serializer.data)
