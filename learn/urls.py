3c4b413ee212b303f50f7b79cd2299802db38d11
halyna-baklanova <novabalka@gmail.com>
2025-06-17 20:39:38 +0300

    create new path fpr upload

--- learn/urls.py
+++ learn/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from learn.views import TaskViewSet, RandomQuestionView, random_question_page
+from learn.views import (
+    TaskViewSet,
+    RandomQuestionView,
+    random_question_page,
+    upload_questions_view
+)
 
 router = DefaultRouter()
 router.register(r"learn", TaskViewSet)
@@ -8,6 +13,7 @@ router.register(r"learn", TaskViewSet)
 urlpatterns = [
     path("", include(router.urls)),
     path("random/", RandomQuestionView.as_view(), name="random-question"),
+    path("upload/", upload_questions_view, name="upload-questions"),
     path("random/question/", random_question_page, name="random-question-html-page")
 ]
 
