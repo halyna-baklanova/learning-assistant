87ced6d865d9cd54947774096fb4107a2d54dfb0
Halyna Baklanova <145159478+halyna-baklanova@users.noreply.github.com>
2025-11-30 16:42:11 +0200

    create admin models for Task, Question

--- learn/admin.py
+++ learn/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Task, Question
 
-# Register your models here.
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'description')
+    search_fields = ('title',)
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'task', 'text', 'answer')
+    list_filter = ('task',)
+    search_fields = ('text',)
