647a211dcb0353459c84db7d2656e9ed94169c6e
Halyna Baklanova <145159478+halyna-baklanova@users.noreply.github.com>
2025-11-30 16:45:42 +0200

    modeified urls

--- learn/urls.py
+++ learn/urls.py
@@ -8,10 +8,11 @@ from learn.views import (
 )
 
 router = DefaultRouter()
-router.register(r"task", TaskViewSet, basename="task")
+router.register(r"tasks", TaskViewSet, basename="task")
 
 urlpatterns = [
     path("", include(router.urls)),
+
     path("random/", RandomQuestionView.as_view(), name="random-question"),
     path("upload/", upload_questions_view, name="upload-questions"),
     path("random/question/", random_question_page, name="random-question-html-page"),
