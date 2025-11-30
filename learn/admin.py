9b3e25ba16fb265a90345e3be555814869de379b
Halyna Baklanova <145159478+halyna-baklanova@users.noreply.github.com>
2025-11-30 16:43:48 +0200

    fix  admin models for Task, Question

--- learn/admin.py
+++ learn/admin.py
@@ -3,11 +3,11 @@ from .models import Task, Question
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'description')
-    search_fields = ('title',)
+    list_display = ("id", "title", "description")
+    search_fields = ("title",)
 
 @admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
-    list_display = ('id', 'task', 'text', 'answer')
-    list_filter = ('task',)
-    search_fields = ('text',)
+    list_display = ("id", "task", "text", "answer")
+    list_filter = ("task",)
+    search_fields = ("text",)
