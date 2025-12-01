bdcd212c08be6e2fbab71340903be1a4f0cf3b6b
Halyna Baklanova <145159478+halyna-baklanova@users.noreply.github.com>
2025-12-01 08:32:30 +0200

    migrate models

--- learn/models.py
+++ learn/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Task(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, default="Untitled")
     description = models.TextField(blank=True, null=True)
 
     def __str__(self):
